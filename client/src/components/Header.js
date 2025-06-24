d3496072a64ae5309bf69b6dc1fa79c79feb3036
Sebastian Cuneo <sebastian.cuneo.s1@gmail.com>
2025-06-24 13:38:41 -0300

    l

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="header">
       <Link to="/" className="logo">
-        GreenMarket
+        GreenMarket 
       </Link>
       <nav>
         <ul className="nav-list">
